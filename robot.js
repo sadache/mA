e7108e9555df2fe5c788801e7dff785d0fa92b02
Guillaume Bort <guillaume.bort@gmail.com>
2011-04-28 23:59:03 +0200

    Add status

--- robot.js
+++ robot.js
@@ -3,12 +3,13 @@ setMessageListener(function(type, data) {
     //console.log(data.others)
 
     postGame('command', {
+        status: 'Searching opponent',
         engine: {
-            left: 0,
+            left: 5,
             right: 0
         },
         tower: 0,
-        fire: 0
+        fire: 5
     })
 
 })
